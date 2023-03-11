3de1b96f560ade2077e8bd87b2b0362a67a51e23
roman10rb <roma10rb@gmail.com>
2023-03-11 20:07:39 +0200

    ready 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,44 @@
+import trottle from 'lodash.throttle';
+
+const STORRAGE_KEY = 'feedback-form-state';
+
+const refs = {
+    form: document.querySelector('form'),
+    input: document.querySelector('input'),
+    textarea: document.querySelector('textarea'),
+};
+refs.form.addEventListener('submit', onFormSubmit );
+refs.form.addEventListener('input', trottle(onInputMessage, 500) );
+
+populateTextarea();
+
+function onFormSubmit(event){
+    event.preventDefault();
+    console.log('Отправляем форму');
+
+    event.target.reset();
+    localStorage.removeItem(STORRAGE_KEY);
+};
+
+const formData = {};
+
+function onInputMessage(event) {
+
+    formData[event.target.name] = event.target.value;
+
+    localStorage.setItem(STORRAGE_KEY, JSON.stringify(formData));
+};
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORRAGE_KEY);
+    if (savedMessage) {
+        const localtorrageMessage = localStorage.getItem(STORRAGE_KEY);
+        const objMessage = JSON.parse(localtorrageMessage);
+        console.log(objMessage);
+        refs.textarea.value = objMessage.message;
+        refs.input.value = objMessage.email;
+    };
+    
+};
+
 
