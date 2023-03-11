1d4dd0e3e804d4c6ab4c7efe57bb775cb0c5e47f
roman10rb <roma10rb@gmail.com>
2023-03-11 14:47:10 +0200

    ready 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,26 @@
+import Player from '@vimeo/player';
+import trottle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+    player.on('play', function(e) {    
+    });
+    
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+
+player.on('timeupdate', trottle(getCurrentTime, 1000));
+
+function getCurrentTime(time) {
+    const timeToPlay = localStorage.setItem('videoplayer-current-time', time.seconds);
+    return timeToPlay;
+};
+const currentTime = localStorage.getItem('videoplayer-current-time') ? localStorage.getItem('videoplayer-current-time') : 0;
+
+player.setCurrentTime(currentTime);
+
+
 
